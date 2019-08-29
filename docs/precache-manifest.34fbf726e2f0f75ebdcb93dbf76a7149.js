868f12384e56e3c98d2eaa6365997fd8e1f7d033
Harry Yu <hi@harryyu.me>
2019-08-29 19:37:48 +1200

    feat: improve build

---
+++ docs/precache-manifest.34fbf726e2f0f75ebdcb93dbf76a7149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c332e6ff1059b5786cbc257480b13a7",
+    "url": "/CDN/index.html"
+  },
+  {
+    "revision": "78c8a44ea4b00221d221",
+    "url": "/CDN/static/css/main.0a48f33c.chunk.css"
+  },
+  {
+    "revision": "eacf04fcaca573e48e25",
+    "url": "/CDN/static/js/2.34e8d642.chunk.js"
+  },
+  {
+    "revision": "78c8a44ea4b00221d221",
+    "url": "/CDN/static/js/main.82a48f72.chunk.js"
+  },
+  {
+    "revision": "f7802cb410a568f1a820",
+    "url": "/CDN/static/js/runtime~main.6c009b46.js"
+  }
+]);
